90db81ee2d8cd7090346421fc9371c67f9ebc273
biaaimi <biagioni.aimi@gmail.com>
2021-11-05 22:04:59 -0300

    negativo

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace negativo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Digite um numero:");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero < 0)
+            {
+            Console.WriteLine("Você digitou um número negativo.");
+            }
+            else if (numero > 0)
+            {
+            Console.WriteLine("Você digitou um número positivo.");
+            }
+        }
+    }
+}
